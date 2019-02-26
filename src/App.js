728eab3bf5918af64e8eb522e17e0b931c13841e
Matthew Davis <matthew.davis@pt-informatics.com>
2019-02-26 14:27:36 +0000

    Move to GitHub

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as Wrld from 'wrld.js';
+
 import './App.css';
 
+const wrldApiKey = '682113b50aa4ddb55c1d9d81175bf6f5';
+const abertay = [56.46335, -2.97490];
+
 class App extends Component {
+
+  componentDidMount() {
+    Wrld.map('map', wrldApiKey, {
+      // These are the options that are referred to at: https://www.wrld3d.com/wrld.js/latest/docs/api/L.Wrld.map/
+      center: abertay,
+      zoom: 18
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+        <div className='App'>
+          <header className='App-header'>
+
+            <div id='overlay' className='wrld-widget-container'>&nbsp;</div>
+            <div id='map'>&nbsp;</div>
+
+          </header>
+        </div>
+
     );
   }
 }

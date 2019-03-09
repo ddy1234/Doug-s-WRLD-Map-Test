2b285bb8cb0d7a9cde7e6390d0a3c2ceaa24d3ae
Doug <ddy1234@gmail.com>
2019-03-09 13:24:08 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import * as Wrld from 'wrld.js';
 import './App.css';
 
 const wrldApiKey = '534b3767557e7806de79d87b1b400de7';
-const abertay = [56.46335, -2.97490];
+const pullar = [56.397824, -3.431702];
 
 class App extends Component {
 
   componentDidMount() {
     Wrld.map('map', wrldApiKey, {
       // These are the options that are referred to at: https://www.wrld3d.com/wrld.js/latest/docs/api/L.Wrld.map/
-      center: abertay,
-      zoom: 18
+      center: pullar,
+      zoom: 17
     });
   }
 
